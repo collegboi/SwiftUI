9f6146c14d31a34e503aa634b073eddee5bfd3da
timothybarnard <tim@megsupporttools.com>
2019-06-04 10:48:45 +0200

    my first SwiftUI project

---
+++ Landmarks/Landmarks/Views/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Timothy Barnard on 04/06/2019.
+//  Copyright © 2019 Timothy Barnard. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(radius: 10)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
---
+++ Landmarks/Landmarks/Views/MapView.swift
@@ -0,0 +1,31 @@
+//
+//  MpView.swift
+//  Landmarks
+//
+//  Created by Timothy Barnard on 04/06/2019.
+//  Copyright © 2019 Timothy Barnard. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        return MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        return uiView.setRegion(region, animated: true)
+    }
+}
+
+#if DEBUG
+struct MpView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
